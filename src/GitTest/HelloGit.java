ec106ce25a308ccc0b0957eb11df0606137ccecd
Therabidmonkey <Therabidmonkey@gmail.com>
2020-09-01 15:56:15 -0400

    First push setting the project up.

---
+++ src/GitTest/HelloGit.java
@@ -0,0 +1,12 @@
+package GitTest;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Add your print statement with your signature and push this to the repository
+		
+		System.out.println("Hello Git -AF");
+
+	}
+
+}
