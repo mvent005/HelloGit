96ff64327294804d8cb1cb5f2b8be751fa4c69aa
Therabidmonkey <Therabidmonkey@gmail.com>
2020-09-12 15:39:05 -0400

    Testing a quick commit

--- src/GitTest/HelloGit.java
+++ src/GitTest/HelloGit.java
@@ -5,8 +5,8 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Add your print statement with your signature and push this to the repository
 		
-		System.out.println("Hello Git -AF");
-		system.out.println("Hello World - AC");
+		System.out.println("Hellochange Git -AF");
+		System.out.println("Hello World - AC");
 
 	}
 
